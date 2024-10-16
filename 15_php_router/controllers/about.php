f502f5bbabdcb05a45269c3648740f7b0b7d5339
Apurv Prajapati <apurv.prajapati27@gmail.com>
2024-10-16 16:42:27 +0530

    Added code in about.php file

--- 15_php_router/controllers/about.php
+++ 15_php_router/controllers/about.php
@@ -1,11 +1,5 @@
-<?php require('partials/head.php') ?>
-<?php require('partials/nav.php') ?>
-<?php require('partials/banner.php') ?>
+<?php
 
-<main>
-    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <p>Hello. Welcome to the about page.</p>
-    </div>
-</main>
+$heading = 'About Us';
 
-<?php require('partials/footer.php') ?>
+require "views/about.view.php";
