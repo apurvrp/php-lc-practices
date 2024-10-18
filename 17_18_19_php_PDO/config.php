d094d3076b6ea8d8b5676638d64421d288aa1ddc
Apurv Prajapati <apurv.prajapati27@gmail.com>
2024-10-18 15:29:15 +0530

    changes in config.php in notes page repo

--- 17_18_19_php_PDO/config.php
+++ 17_18_19_php_PDO/config.php
@@ -4,7 +4,7 @@ return [
     'database' => [
         'host' => 'localhost',
         'port' => 3306,
-        'dbname' => 'myapp',
+        'dbname' => 'php_practices_01',
         'charset' => 'utf8mb4'
     ],
 
