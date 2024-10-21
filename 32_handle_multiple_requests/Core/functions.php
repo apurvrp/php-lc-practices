b8381b1fef4866768bc3985ca6f180bae3a28285
Apurv Prajapati <apurv.prajapati27@gmail.com>
2024-10-21 18:18:41 +0530

    Create repository of handle multiple requests methods

---
+++ 32_handle_multiple_requests/Core/functions.php
@@ -0,0 +1,36 @@
+<?php
+
+use Core\Response;
+
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+function urlIs($value)
+{
+    return $_SERVER['REQUEST_URI'] === $value;
+}
+
+function authorize($condition, $status = Response::FORBIDDEN)
+{
+    if (! $condition) {
+        abort($status);
+    }
+}
+
+function base_path($path)
+{
+    return BASE_PATH . $path;
+}
+
+function view($path, $attributes = [])
+{
+    extract($attributes);
+
+    require base_path('views/' . $path);
+}
