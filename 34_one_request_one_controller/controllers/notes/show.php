12ed8a8846596b3b6f69bfa7b0d6616b0647ac9c
Apurv Prajapati <apurv.prajapati27@gmail.com>
2024-10-21 20:08:24 +0530

    Create new repository for one request one controller

---
+++ 34_one_request_one_controller/controllers/notes/show.php
@@ -0,0 +1,34 @@
+<?php
+
+use Core\Database;
+
+$config = require base_path('config.php');
+$db = new Database($config['database']);
+
+$currentUserId = 1;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    $db->query('delete from notes where id = :id', [
+        'id' => $_GET['id']
+    ]);
+
+    header('location: /notes');
+    exit();
+} else {
+    $note = $db->query('select * from notes where id = :id', [
+        'id' => $_GET['id']
+    ])->findOrFail();
+
+    authorize($note['user_id'] === $currentUserId);
+
+    view("notes/show.view.php", [
+        'heading' => 'Note',
+        'note' => $note
+    ]);
+}
