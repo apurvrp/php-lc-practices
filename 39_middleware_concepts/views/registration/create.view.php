2b8e7ac8f08dcaaef63c0c89a2945eb8e0685a1b
Apurv Prajapati <apurv.prajapati27@gmail.com>
2024-10-22 16:07:30 +0530

    Create new repository for middleware concepts

---
+++ 39_middleware_concepts/views/registration/create.view.php
@@ -0,0 +1,55 @@
+<?php require base_path('views/partials/head.php') ?>
+<?php require base_path('views/partials/nav.php') ?>
+
+<main>
+    <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div class="w-full max-w-md space-y-8">
+            <div>
+                <svg viewBox="0 0 70 31" class="mx-auto h-12 w-auto">
+                    <path fill-rule="evenodd" clip-rule="evenodd"
+                        d="M25.517 0C18.712 0 14.46 3.382 12.758 10.146c2.552-3.382 5.529-4.65 8.931-3.805 1.941.482 3.329 1.882 4.864 3.432 2.502 2.524 5.398 5.445 11.722 5.445 6.804 0 11.057-3.382 12.758-10.145-2.551 3.382-5.528 4.65-8.93 3.804-1.942-.482-3.33-1.882-4.865-3.431C34.736 2.92 31.841 0 25.517 0zM12.758 15.218C5.954 15.218 1.701 18.6 0 25.364c2.552-3.382 5.529-4.65 8.93-3.805 1.942.482 3.33 1.882 4.865 3.432 2.502 2.524 5.397 5.445 11.722 5.445 6.804 0 11.057-3.381 12.758-10.145-2.552 3.382-5.529 4.65-8.931 3.805-1.941-.483-3.329-1.883-4.864-3.432-2.502-2.524-5.398-5.446-11.722-5.446z"
+                        fill="#38bdf8"></path>
+                </svg>
+                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Register for a new
+                    account</h2>
+            </div>
+
+            <form class="mt-8 space-y-6" action="/register" method="POST">
+                <div class="-space-y-px rounded-md shadow-sm">
+                    <div>
+                        <label for="email" class="sr-only">Email address</label>
+                        <input id="email" name="email" type="email" autocomplete="email" required
+                            class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Email address">
+                    </div>
+
+                    <div>
+                        <label for="password" class="sr-only">Password</label>
+                        <input id="password" name="password" type="password" autocomplete="current-password" required
+                            class="relative block w-full appearance-none rounded-none rounded-b-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            placeholder="Password">
+                    </div>
+                </div>
+
+                <div>
+                    <button type="submit"
+                        class="group relative flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">
+                        Register
+                    </button>
+                </div>
+
+                <ul>
+                    <?php if (isset($errors['email'])) : ?>
+                    <li class="text-red-500 text-xs mt-2"><?= $errors['email'] ?></li>
+                    <?php endif; ?>
+
+                    <?php if (isset($errors['password'])) : ?>
+                    <li class="text-red-500 text-xs mt-2"><?= $errors['password'] ?></li>
+                    <?php endif; ?>
+                </ul>
+            </form>
+        </div>
+    </div>
+</main>
+
+<?php require base_path('views/partials/footer.php') ?>
