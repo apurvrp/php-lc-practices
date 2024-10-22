61354b049a5a07d341bb5deadbe4c62b8049ae28
Apurv Prajapati <apurv.prajapati27@gmail.com>
2024-10-22 12:58:00 +0530

    create Patch request and their respective changes

--- 36_create_patch_request/routes.php
+++ 36_create_patch_request/routes.php
@@ -8,5 +8,8 @@ $router->get('/notes', 'controllers/notes/index.php');
 $router->get('/note', 'controllers/notes/show.php');
 $router->delete('/note', 'controllers/notes/destroy.php');
 
+$router->get('/note/edit', 'controllers/notes/edit.php');
+$router->patch('/note', 'controllers/notes/update.php');
+
 $router->get('/notes/create', 'controllers/notes/create.php');
 $router->post('/notes', 'controllers/notes/store.php');
