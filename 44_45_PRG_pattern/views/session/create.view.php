28b2129217ccd83b4d63359e510b79901033e4d4
Apurv Prajapati <apurv.prajapati27@gmail.com>
2024-10-23 16:01:04 +0530

    Changes in function.php and others files

--- 44_45_PRG_pattern/views/session/create.view.php
+++ 44_45_PRG_pattern/views/session/create.view.php
@@ -19,7 +19,7 @@
                         <label for="email" class="sr-only">Email address</label>
                         <input id="email" name="email" type="email" autocomplete="email" required
                             class="relative block w-full appearance-none rounded-none rounded-t-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                            placeholder="Email address">
+                            placeholder="Email address" value="<?= old('email') ?>">
                     </div>
 
                     <div>
