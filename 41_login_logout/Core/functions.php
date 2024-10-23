d55f92f1f8b2bc57ec9c01639d2f39e17bfb2a77
Apurv Prajapati <apurv.prajapati27@gmail.com>
2024-10-23 19:39:08 +0530

    Issues resolved in logout - resolved #36

--- 41_login_logout/Core/functions.php
+++ 41_login_logout/Core/functions.php
@@ -46,21 +46,13 @@ function view($path, $attributes = [])
     require base_path('views/' . $path);
 }
 
-function login($user)
+function redirect($path)
 {
-    $_SESSION['user'] = [
-        'email' => $user['email']
-    ];
-
-    session_regenerate_id(true);
+    header("location: {$path}");
+    exit();
 }
 
-function logout()
+function old($key, $default = '')
 {
-    $_SESSION = [];
-    session_destroy();
-
-    $params = session_get_cookie_params();
-    setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
-
+    return Core\Session::get('old')[$key] ?? $default;
 }
